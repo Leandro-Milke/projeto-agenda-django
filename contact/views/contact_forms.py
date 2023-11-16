19f663901c0134a3d351c35c446ced9f130294e9
Leandro Milke Cavalheiro <leandromilkecavalheiro@gmail.com>
2023-11-16 16:15:45 -0300

    Criado forms e lógica  do template  CREATE

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,11 +3,30 @@ from contact.models import Contact
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django import forms
+
+class ContactForms(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
 
 def create(request):
-   
+    if request.method == 'POST':
+        context = {
+        'form': ContactForms(request.POST)
+         }
+
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+ 
 
     context = {
+        'form': ContactForms()
         
     }
 
