b659ebbfb489ccd88989d5fb8b4bacce799af14d
Leandro Milke Cavalheiro <leandromilkecavalheiro@gmail.com>
2023-11-20 11:27:32 -0300

    Criado wiew para update

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,25 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForms
+from django.urls import reverse
+from contact.models import Contact
 
 
 
 def create(request):
+    form_action = reverse('contact:create')
+
     if request.method == 'POST':
+        form = ContactForms(request.POST)
+
         context = {
-        'form': ContactForms(request.POST)
+        'form': form,
+        'form_action': form_action,
          }
+        
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id = contact.id)
 
         return render(
             request,
@@ -18,7 +29,45 @@ def create(request):
  
 
     context = {
-        'form': ContactForms()
+        'form': ContactForms(),
+        'form_action': form_action,
+        
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context,
+    )
+
+def update(request,contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    form_action = reverse('contact:update',args= (contact_id,))
+
+    if request.method == 'POST':
+        form = ContactForms(request.POST, instance =contact)
+
+        context = {
+        'form': form,
+        'form_action': form_action,
+         }
+        
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id = contact.id)
+
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+ 
+
+    context = {
+        'form': ContactForms( instance =contact),
+        'form_action': form_action,
         
     }
 
