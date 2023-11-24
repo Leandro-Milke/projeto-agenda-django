8a845e84f4ee2c89bb391edcc492a03dc89aec9a
Leandro Milke Cavalheiro <leandromilkecavalheiro@gmail.com>
2023-11-24 16:58:22 -0300

    criado mecanismo para registro, login e logout d uusuarios

--- contact/forms.py
+++ contact/forms.py
@@ -1,12 +1,9 @@
 from collections.abc import Mapping
-from typing import Any
 from django import forms
 from django.core.exceptions import ValidationError
-from django.core.files.base import File
-from django.db.models.base import Model
-from django.forms.utils import ErrorList
 from . import models
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForms(forms.ModelForm):
 
@@ -43,3 +40,32 @@ class ContactForms(forms.ModelForm):
             )
 
         return first_name
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+    
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+        return email
